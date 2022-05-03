4f2764313b136afac893ef27215a542091243282
TravisNickels <87037242+TravisNickels@users.noreply.github.com>
2022-05-03 15:38:29 -0700

    Update context

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const context = github.context.payload;
+    const context = github.context;
     const owner = core.getInput('repositoryOwner');
     const repository = core.getInput('repository');
     const octokit = github.getOctokit(
         core.getInput('repoToken', { required: true })
     );
+
+    app.log.info("I see the issue_comment.created");
 
     const issueComment = context.issue({
         body: "This is from the GHA",
