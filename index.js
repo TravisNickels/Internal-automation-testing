553f32c74a78a05459a21edcaf5eb155f6b7e9d8
TravisNickels <87037242+TravisNickels@users.noreply.github.com>
2022-05-03 18:47:47 -0700

    trying to stop the double comment

--- index.js
+++ index.js
@@ -1,51 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-console.log("before run");
-
-function getIssueProps(context) {
-    return {
-        owner: context.repository.owner,
-        repo: context.repository.name,
-        issue_number: context.issue.number,
-    };
-}
-  
-function createComment(octokit, context, body) {
-    return octokit.rest.issues.createComment({
-        ...getIssueProps(context),
-        body,
-    });
-}
-
 async function run() {
-    const context = github.context.payload;
-    const owner = core.getInput('repositoryOwner');
-    const repository = core.getInput('repository');
-    const octokit = github.getOctokit(
-        core.getInput('repoToken', { required: true })
-    );
+    try {
+        if (!isPost) {
+            const context = github.context.payload;
+            const owner = core.getInput('repositoryOwner');
+            const repository = core.getInput('repository');
+            const octokit = github.getOctokit(
+                core.getInput('repoToken', { required: true })
+            );
 
-    context.repository = {
-        owner,
-        name: repository.split('/')[1],
-    };
+            context.repository = {
+                owner,
+                name: repository.split('/')[1],
+            };
 
-    console.log("I see the issue_comment.created");
+            console.log("Creating the comment");
 
-    // await createComment(
-    //     octokit,
-    //     context,
-    //     "I see the comment you made!"
-    // );
-
-    await octokit.rest.issues.createComment({
-        owner: context.repository.owner,
-        repo: context.repository.name,
-        issue_number: context.issue.number,
-        body: "I got the comment on the issue! ✅"
-    })
-    //await octokit.rest.issues.createComment(issueComment)
+            await octokit.rest.issues.createComment({
+                owner: context.repository.owner,
+                repo: context.repository.name,
+                issue_number: context.issue.number,
+                body: "I got the comment on the issue! ✅"
+            })
+        } else { // Cleanup
+            console.log("Running cleanup");
+        }
+    } catch (err) {
+        core.setFailed(err);
+        console.log(err);
+    } 
 }
 
 run();
